121bf6c2ddac7c5e78144c9d6139c3241030bf45
sgksm85 <sgksm85@gmail.com>
2024-10-25 11:16:49 +0900

    Update redirect URI for Vercel deployment

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
 export async function init() {
   miro.board.ui.on('icon:click', async () => {
-    await miro.board.ui.openPanel({url: 'app.html'});
+    await miro.board.ui.openPanel({ url: 'app.html' });
   });
 }
 
-init();
+// MiroのSDKが読み込まれた後に初期化を実行
+miro.onReady(() => {
+  init();
+});
