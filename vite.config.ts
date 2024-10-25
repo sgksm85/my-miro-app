68307c949eb8f9a16bc16fe9f749da0ffe997a93
sgksm85 <sgksm85@gmail.com>
2024-10-25 11:59:39 +0900

    Update redirect URI for Vercel deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import fs from 'fs';
 import dns from 'dns';
 import {defineConfig} from 'vite';
 import react from '@vitejs/plugin-react';
-
 // https://vitejs.dev/config/server-options.html#server-host
 dns.setDefaultResultOrder('verbatim');
 
@@ -19,13 +18,17 @@ const allHtmlEntries = fs
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    rollupOptions: {
-      input: allHtmlEntries,
-    },
-  },
   plugins: [react()],
   server: {
-    port: 3000,
+    port: 3000
   },
+  // SPAのための設定を追加
+  base: '/',
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: undefined
+      }
+    }
+  }
 });
